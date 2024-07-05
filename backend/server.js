748ecd74440f4b75fe89448a59dfe5c776d027a8
kunal Dugar <yoyokvunal@gmail.com>
2024-07-05 16:46:04 +0530

    Changes made to deploy the application

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import { v2 as cloudinary } from "cloudinary";
+import path from "path";
 
 import authRoutes from "./routes/authRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
@@ -20,6 +21,7 @@ cloudinary.config({
 });
 
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: "10mb" })); //to parse req.body in json format
 app.use(express.urlencoded({ extended: true })); //to parse req.body in urlencoded format
@@ -28,6 +30,13 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.get("/health", (req, res) => {
   res.send("Server is perfectly running without any issues");
